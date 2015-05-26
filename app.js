c515ece5507f95940cc3e3c3649a4a49436949af
Andrew Tamura <andrew@mixmax.com>
2015-05-26 10:48:03 -0700

    Better layout for serving static files

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var serveStatic = require('serve-static')
 var app = express();
 
 app.use(require('connect-livereload')());
 app.listen('8888');
+
+app.use(serveStatic('challenges', {index: ['index.html']}));
