224d5680cddcb8f8dd090ac4ea003cc0d7e137a7
Jeff Wear <jeff@mixmax.com>
2015-05-26 11:48:29 -0700

    Serve the challenges.

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ var express = require('express');
 var app = express();
 
 app.use(require('connect-livereload')());
+
+app.use(express.static(__dirname, {
+  // Don't cache anything.
+  maxAge: 0
+}));
+
 app.listen('8888');
