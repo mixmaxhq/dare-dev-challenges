881ab5a7bcf9bdf625aaf908ce1f009544d1e4b9
Andrew Tamura <andrew@mixmax.com>
2015-05-26 14:57:19 -0700

    preview page loads the last changed challenge in an iframe

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use(require('connect-livereload')({ port: 19999 }));
+app.use('/preview', function(req, res, next) {
+  res.sendFile('preview.html', {
+    root: __dirname + '/src/',
+    dotfiles: 'deny',
+  });
+});
 
 app.use(express.static(__dirname, {
   // Don't cache anything.
